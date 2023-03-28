6b9ba7796298074f24f0301bbc7a736b14188176
raunakwete43 <104648854+raunakwete43@users.noreply.github.com>
2023-03-28 21:47:19 +0530

    Add files via upload

---
+++ live.py
@@ -0,0 +1,25 @@
+import cv2
+from face_recognition import face_locations
+from simple_facerec import SimpleFacerec
+
+sfr = SimpleFacerec()
+sfr.load_encoding_images("/mnt/92205AA6205A9159/Projects/Python/face_ai/photos/")
+
+cap = cv2.VideoCapture(0)
+
+while True :
+    ret, frame = cap.read()
+    face_locations, face_names = sfr.detect_known_faces(frame)
+    for face_loc, name in zip(face_locations, face_names):
+        y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+        cv2.putText(frame, name, (x1, y1-10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 2)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 2)
+
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27 :
+        break
+
+cap.release()
+cv2.destroyAllWindows()
